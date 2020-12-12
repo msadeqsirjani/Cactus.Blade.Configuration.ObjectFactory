605d34fe14f4a87038d89f2725d70c0d18bd336a
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2020-12-12 10:00:46 +0330

    Create Member.cs

---
+++ Configuration.ObjectFactory/Member.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Cactus.Blade.Configuration.ObjectFactory
+{
+    public class Member
+    {
+        public Member(string name, Type type, MemberType memberType)
+        {
+            Name = name;
+            Type = type;
+            MemberType = memberType;
+        }
+
+        public string Name { get; }
+        public Type Type { get; }
+        public MemberType MemberType { get; }
+
+        public override string ToString()
+        {
+            return $"{(MemberType == MemberType.Property ? "Property" : "Constructor parameter")}: {Type} {Name}";
+        }
+    }
+}
